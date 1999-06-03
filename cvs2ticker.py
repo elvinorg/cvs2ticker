7cbe6cfb2b36c6d815ea9ebed37470a8a61133d4
David Arnold <d@0x1.org>
1999-06-03 10:45:42 +1000

    Initial revision

---
+++ cvs2ticker.py
@@ -0,0 +1,282 @@
+#! /usr/bin/env python
+########################################################################
+#
+#              Tickertape
+#              cvs loginfo producer
+#
+# File:        $Source: /home/d/work/personal/ticker-cvs/cvs2ticker/cvs2ticker.py,v $
+# Version:     $RCSfile: cvs2ticker.py,v $ $Revision: 1.1 $
+# Copyright:   (C) 1998-1999, David Leonard, Bill Segall & David Arnold.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+########################################################################
+'''
+
+cvs2ticker - pass CVS loginfo messages through to tickertape
+
+To enable this you should 'cvs co CVSROOT' then add the following line
+to the file named 'loginfo':
+   	
+	ALL	<path>/cvs2ticker %s [-g group]
+
+Then update your ~/.ticker/groups file to include the group which
+defaults to 'CVS', and all CVS updates will scroll by thereafter.
+
+'''
+__author__ = 'David Leonard <david.leonard@dstc.edu.au>'
+__version__ = "$Revision: 1.1 $"[11:-2]
+
+
+########################################################################
+
+import Elvin, ElvinMisc
+import base64, os, pickle, sys, getopt, random, string, time
+
+
+########################################################################
+
+DEFAULT_GROUP = 'CVS'
+TIMEOUT = 10
+
+LOG_MESSAGE    = 'Log Message:'
+MODIFIED_FILES = 'Modified Files:'
+ADDED_FILES    = 'Added Files:'
+REMOVED_FILES  = 'Removed Files:'
+TEXT_UPDATE    = 'Update of '
+TEXT_INDIR     = 'In directory '
+
+VENDOR_TAG     = 'Vendor Tag:'
+VENDOR_KEY     = 'Vendor-Tag'
+RELEASE_TAG    = 'Release Tags:'
+RELEASE_KEY    = 'Release-Tags'
+
+TEXT_IMPORT    = 'cvs import:'
+STATUS         = 'Status:'
+IMPORTED_KEY   = 'Imported-Files'
+
+d_section     = {LOG_MESSAGE:    "Log-Message",
+                 MODIFIED_FILES: "Modified-Files", 
+                 ADDED_FILES:    "Added-Files",
+                 REMOVED_FILES:  "Removed-Files",
+                 STATUS:         "Status",
+                 }
+
+
+
+########################################################################
+
+def log_to_ticker(l_args):
+    '''Read the loginfo data from standard input and args and
+    return a short bit of text that captures this.'''
+
+    #-- initialise parsed info
+    d_notify = {}
+    cur_section = None
+    extratext = ""
+
+    for key in d_section.values():
+        d_notify[key] = ""
+
+    #-- read and process log message
+    lines = sys.stdin.readlines()
+    for line in lines:
+        
+        #-- remove trailing newline
+        if line[-1] == "\n":
+            line = line[:-1]
+
+        #-- skip blank lines
+        if not string.strip(line):
+            continue
+
+        if line in d_section.keys():
+            #-- handle multi-line sections
+            cur_section = d_section[line]
+        
+        elif line[0] == "\t":
+            d_notify[cur_section] = d_notify[cur_section] + ' ' + string.strip(line)
+
+        elif string.find(line, VENDOR_TAG) == 0:
+            cur_section = None
+            d_notify[VENDOR_KEY] = line[len(VENDOR_TAG)+1:]
+
+        elif string.find(line, RELEASE_TAG) == 0:
+            cur_section = None
+            if not d_notify.has_key(RELEASE_TAG):
+                d_notify[RELEASE_KEY] = line[len(RELEASE_TAG)+1:]
+            else:
+                d_notify[RELEASE_KEY] = d_notify[RELEASE_KEY] + " " + string.strip(line)
+            
+        elif cur_section == d_section[LOG_MESSAGE]:
+            d_notify[cur_section] = d_notify[cur_section] + ' ' + line
+        
+        else:
+            cur_section = None
+
+            #-- process remaining lines
+            if string.find(line, TEXT_INDIR) == 0:
+                d_notify["Working-Directory"] = line[len(TEXT_INDIR):]
+
+            elif string.find(line, TEXT_UPDATE) == 0:
+                d_notify["Repository-Directory"] = line[len(TEXT_UPDATE):]
+
+            elif line[0:2] == "N ":
+                if not d_notify.has_key(IMPORTED_KEY):
+                    d_notify[IMPORTED_KEY] = line[2:]
+                else:
+                    d_notify[IMPORTED_KEY] = d_notify[IMPORTED_KEY] + ' ' + line[2:]
+                
+            elif line[0:2] == "I ":
+                #-- ignore these, since CVS does ...
+                pass
+            
+            else:
+                extratext = extratext + ' ' + line
+
+
+    #-- create tickertape message
+    msg = ""
+    
+    if d_notify[d_section[ADDED_FILES]]:
+	msg = msg + " Added " + d_notify[d_section[ADDED_FILES]]
+        
+    if d_notify[d_section[REMOVED_FILES]]:
+	msg = msg + " Removed " + d_notify[d_section[REMOVED_FILES]]
+
+    if d_notify[d_section[MODIFIED_FILES]]:
+	msg = msg + " Modified " + d_notify[d_section[MODIFIED_FILES]]
+
+    if d_notify.has_key(IMPORTED_KEY):
+        msg = msg + " Import"
+        
+    #-- the bill trap
+    if not string.strip(string.replace(d_section[LOG_MESSAGE], "\n", " ")):
+        d_section[LOG_MESSAGE] = "%s, the slack bastard, didn't supply " \
+                            "a log message." % user
+
+    msg = msg + ':' + d_notify[d_section[LOG_MESSAGE]]
+
+    #-- add non-parsed text
+    d_notify["Extras"] = extratext
+    d_notify["Original"] = str(lines)
+    
+    #-- create attachment URL
+    str_url = "http://internal.dstc.edu.au/cgi-bin/cvs2web.py"
+    str_url = str_url + "?%s+%s" % (user, url_escape(pickle.dumps(d_notify)))
+
+    #-- add tickertape-specific attributes
+    d_notify.update({'TIMEOUT' : TIMEOUT,
+		    'TICKERTEXT' : msg,
+		    'TICKERTAPE' : group,
+		    'USER' : user,
+                    'MIME_TYPE':   "x-elvin/url",
+                    'MIME_ARGS':   str_url,
+                    'Message-Id':  random.randint(1, 0x7ffffff),
+                    'In-Reply-To': 0})
+    return d_notify
+
+
+def url_escape(s):
+    """Escape characters in s for inclusion in an URL"""
+
+    s = base64.encodestring(s)
+    s = string.replace(s, '\012', r'\x0a')
+    s = string.replace(s, '=', r'\x3d')
+    s = string.replace(s, '+', r'\x2b')
+    
+    return s
+
+
+########################################################################
+
+if __name__ == '__main__':
+    progname = os.path.basename(sys.argv[0])
+    USAGE = "Usage: %s [-h host] [-p port] [-g group]\n" % (progname,)
+
+    # Parse the args to get the optional host and port, then connect to Elvin
+    ports = []
+    hosts = []
+    groups = []
+    host = None
+    port = None
+    group = None
+
+    try:
+	(optlist,args) = getopt.getopt(sys.argv[1:], "p:h:g:")
+    except:
+	sys.stderr.write(Usage + "Failed to process the arglist\n")
+	print optlist, args
+	sys.exit(1)
+    else:
+	for (opt, arg) in optlist:
+	    if opt == '-h':
+		hosts.append(arg)
+	    if opt == '-p':
+		ports.append(arg)
+	    if opt == '-g':
+		groups.append(arg)
+
+	if len(hosts) == 1:
+	    host = hosts[0]
+	elif len(hosts) > 1:
+	    sys.stderr.write(Usage + "Can only specify one elvin host\n")
+	    sys.exit(1)
+
+	if len(groups) == 0:
+	    group = DEFAULT_GROUP
+	if len(groups) == 1:
+	    group = groups[0]
+	elif len(groups) > 1:
+	    sys.stderr.write(Usage + "Can only specify one group\n")
+	    sys.exit(1)
+
+	if len(ports) == 1:
+	    try:
+		port = string.atoi(ports[0])
+	    except:
+		sys.stderr.write(Usage + "Port must be numeric\n")
+		sys.exit(1)
+	    else:
+		if port < 0:
+		    sys.stderr.write(Usage + "Port must be positive\n")
+		    sys.exit(1)
+	elif len(ports) > 1:
+	    sys.stderr.write(Usage + "Can only specify one elvin port\n")
+	    sys.exit(1)
+	    
+
+    # Fix the host and port to something useful if they didn't tell us
+    (host, port) = ElvinMisc.HostAndPort(host, port)
+    try:
+	e = Elvin.Elvin(Elvin.EC_NAMEDHOST, host, port)
+    except:
+	sys.stderr.write("Unable to connect to Elvin at %s:%d\n" %(host, port))
+
+    user = ElvinMisc.GetUserName()
+
+
+    print "cvs2ticker, v" + __version__ + ":",
+
+    #-- parse log message
+    d_notify = log_to_ticker(args)
+    if d_notify:
+        e.notify(d_notify)
+
+    print "Ok."
+    sys.exit(0)
+
+
+########################################################################
---
+++ cvs2web.py
@@ -0,0 +1,391 @@
+#! /usr/local/dstc/alpha-dec-osfV4.0/bin/python
+#############################################################################
+#
+#              TickerTape
+#              CGI script for cvs2ticker URLs
+#
+# File:        $Source: /home/d/work/personal/ticker-cvs/cvs2ticker/cvs2web.py,v $
+# Version:     $RCSfile: cvs2web.py,v $ $Revision: 1.1 $
+# Copyright:   (C) 1999, David Arnold.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+########################################################################
+"""
+
+this file is a CGI script which produces a download request page for a
+package.  it is generic, in that it takes a number of parameters
+supplying the package name, available formats, etc etc
+
+"""
+__author__  = "David Arnold <davida@pobox.com>"
+__version__ = "$Revision: 1.1 $"[11:-2]
+
+
+#############################################################################
+
+import base64, cgi, os, pickle, popen2, regsub, string, sys, time
+
+
+#############################################################################
+
+def send(txt, indent=0):
+    """Write text to output."""
+    
+    #-- create indent string
+    str_indent = " " * indent
+
+    #-- insert indent after carriage returns
+    txt = regsub.gsub("\n\(.\)", "\n%s\\1" % str_indent, txt)
+
+    #-- write
+    sys.stdout.write("%s%s" % (str_indent, txt))
+    
+    return
+
+
+def wrap(s, width=60):
+    """Return the string with long strings wrapped."""
+
+    res = ""
+    
+    #-- separate into paragraphs
+    lst_para = string.split(s, "\n")
+    for para in lst_para:
+        if len(para) <= width:
+            res = res + para + "\n"
+
+        else:
+            lst_word = string.split(para)
+            line = ""
+            for word in lst_word:
+                if not line:
+                    line = word
+                    
+                elif len(line) + len(word) + 1 <= width:
+                    line = line + " " + word
+                    
+                else:
+                    res = res + line + "\n"
+                    line = word
+                    
+            res = res + line + "\n"
+
+    return res
+
+
+            
+#############################################################################
+
+def header():
+    send("Content-type: text/html\n\n")
+    send("<html>\n")
+    send("<head>\n", 2)
+    send("<title>cvs2ticker</title>\n", 4)
+    send("</head>\n", 2)
+
+    send('<body bgcolor="white">\n', 2)
+    send('<h1><tt>cvs2web, v%s</tt><hr></h1>\n' % __version__, 4)
+    
+    return
+
+
+def end_body():
+    send("<hr>\n", 4)
+    str_time = time.asctime(time.localtime(time.time()))
+    
+    send("<address>%s</address>\n" % str_time, 4)
+    send("</body>\n", 2)
+    send("</html>\n\n")
+    return
+
+
+#############################################################################
+
+def user_info(user, d_cvs):
+    """Show the name of the user who performed the commit"""
+
+    #-- get the name of the person?
+    logname = "cvs%20user"
+
+    #-- determine module name (from working & repository paths)
+    module = "elvin"
+    
+    send('<tt><b>cvs commit</b></tt> by %s' % user, 4)
+    send(' [<a href="mailto:%s@dstc.edu.au">mail</a>]' % user, 4)
+    send(' [<a href="/cgi-bin/ticker.py?%s+10+%s+%s+CVS">ticker</a>]<p>\n' % \
+         (logname, user, module), 4)
+    
+    return
+
+
+def log_msg(d_cvs):
+    if not d_cvs.has_key("Log-Message"):
+        return
+
+    send('<table align="center" width="90%">\n', 4)
+    send('<tr>\n', 6)
+    send('<td bgcolor="#c0c0c0">\n', 8)
+    send("<pre>\n\n", 10)
+    send(wrap(d_cvs["Log-Message"]))
+    send("\n  </pre>\n", 10)
+    send("    </td>\n  </tr>\n</table>", 4)
+    return
+
+
+def add_info(d_cvs):
+
+    if not d_cvs.has_key("Added-Files"):
+        return
+
+    if not d_cvs["Added-Files"]:
+        return
+    
+    send("<dl>\n  <dt>Added files:\n", 4)
+
+    for file in string.split(d_cvs["Added-Files"]):
+        full_path = os.path.join(d_cvs["Repository-Directory"], file)
+
+        send("<dd>%s" % file)
+        send(' [<a href="/cgi-bin/cvs2web.py?file+%s">file</a>]' %full_path, 6)
+        send(' [<a href="/cgi-bin/cvsweb.cgi/%s">cvsweb</a>]' %file, 6)
+        
+    send("</dl>", 4)
+    send("<p>", 4)
+    
+    return
+
+
+def remove_info(d_cvs):
+    if not d_cvs.has_key("Removed-Files"):
+        return
+
+    if not d_cvs["Removed-Files"]:
+        return
+    
+    send("<dl>\n  <dt>Removed files:\n", 4)
+
+    for file in string.split(d_cvs["Removed-Files"]):
+        send("<dd>%s\n" % file)
+        
+    send("</dl>", 4)
+    send("<p>", 4)
+    
+    return
+
+
+def modify_info(d_cvs):
+    if not d_cvs.has_key("Modified-Files"):
+        return
+
+    if not d_cvs["Modified-Files"]:
+        return
+    
+    send("<dl>\n  <dt>Modified files:\n", 4)
+
+    for file in string.split(d_cvs["Modified-Files"]):
+        full_path = os.path.join(d_cvs["Repository-Directory"], file)
+        
+        send('<dd>%s' % file, 6)
+        send(' [<a href="/cgi-bin/cvs2web.py?file+%s">file</a>]' %full_path, 6)
+        send(' [<a href="/cgi-bin/cvs2web.py?diff+%s">diff</a>]' %full_path, 6)
+        send(' [<a href="/cgi-bin/cvs2web.py?log+%s">log</a>]' %full_path, 6)
+        send(' [<a href="/cgi-bin/cvsweb.cgi/%s">cvsweb</a>]' %file, 6)
+        
+    send("\n</dl>\n", 4)
+    send("<p>\n", 4)
+    
+    return
+
+
+def import_info(d_cvs):
+    if not d_cvs.has_key("Imported-Files"):
+        return
+
+    if not d_cvs["Imported-Files"]:
+        return
+
+    str_dir = os.path.dirname(d_cvs["Repository-Directory"])
+    send("<dl>\n  <dt>Imported files:\n", 4)
+
+    for file in string.split(d_cvs["Imported-Files"]):
+        full_path = os.path.join(str_dir, file)
+
+        send('<dd>%s' % file, 6)
+        send(' [<a href="/cgi-bin/cvs2web.py?file+%s">file</a>]' %full_path, 6)
+        send(' [<a href="/cgi-bin/cvs2web.py?log+%s">log</a>]' %full_path, 6)
+        send(' [<a href="/cgi-bin/cvsweb.cgi/%s">cvsweb</a>]' %file, 6)
+
+    send("\n</dl>\n", 4)
+    send("<p>\n", 4)
+    
+    return
+
+
+#############################################################################
+
+def file(str_file):
+    """Return the full source of the named file."""
+
+    send('Content-type: text/plain\n\n')
+
+    try:
+        os.system("cd /tmp; /usr/local/bin/co %s,v" % str_file)
+    
+        f = open("/tmp/%s" % os.path.basename(str_file))
+        s = f.read()
+        f.close()
+
+        if s:
+            send(s)
+
+        else:
+            send("< zero length file >")
+            
+    finally:
+        os.unlink("/tmp/%s" % os.path.basename(str_file))
+        
+    return
+
+
+#############################################################################
+
+def diff(str_file):
+    """Return the diff between the current and last versions."""
+
+    send('Content-type: text/plain\n\n')
+
+    try:
+        #-- check out working file in /tmp
+        os.system("cd /tmp; /usr/local/bin/co %s,v" % str_file)
+
+        #-- run rlog to get latest version number
+        p = os.popen("/usr/local/bin/rlog %s" % str_file, "r")
+        l = p.readlines()
+        p.close()
+
+        rev = l[3][6:-1]
+        major, minor = string.split(rev, ".")
+        prev = "%s.%s" % (major, str(string.atoi(minor) - 1))
+
+        #-- run rcsdiff
+        pout, pin, perr = popen2.popen3("cd /tmp; /usr/local/bin/rcsdiff -r%s %s,v 2>&1" % (prev, str_file))
+        s = pout.read()
+        pout.close()
+        pin.close()
+        perr.close()
+        
+        #-- return output
+        send(s)
+
+    finally:
+        #-- remove checked out working file
+        os.unlink("/tmp/%s" % os.path.basename(str_file))
+
+    return
+
+
+#############################################################################
+
+def log(str_file):
+    """Return the log for the file."""
+
+    send('Content-type: text/plain\n\n')
+    try:
+        p = os.popen("/usr/local/bin/rlog %s" % str_file, "r")
+        s = p.read()
+        p.close()
+
+        send(s)
+
+    finally:
+        sys.exit(0)
+        
+    return
+
+
+#############################################################################
+
+def error(msg):
+    """Print a HTML usage message."""
+    
+    send('Content-type: text/plain\n\n')
+    send('cvs2web, v%s\n' % __version__)
+    send('%s\n\n' % msg)
+
+    send('Required arguments are ...\n')
+    send('user  -- string user name\n')
+    send('dict  -- pickled dictionary of parsed CVS log message\n\n')
+
+    return
+
+        
+#############################################################################
+
+if __name__ == "__main__":
+
+    #-- check parameters
+    if len(sys.argv) != 3:
+        error("Not enough arguments: %d, %s" % (len(sys.argv), str(sys.argv)))
+        sys.exit(0)
+
+    #-- process arguments
+    user = sys.argv[1]
+
+    if user == "log":
+        log(sys.argv[2])
+        sys.exit(0)
+
+    elif user == "diff":
+        diff(sys.argv[2])
+        sys.exit(0)
+
+    elif user == "file":
+        file(sys.argv[2])
+        sys.exit(0)
+
+    elif user == "commit":
+        pass
+
+    else:
+        try:
+            raw_str = string.replace(sys.argv[2], r'\x0a', '\012')
+            raw_str = string.replace(raw_str, r'\x3d', '=')
+            raw_str = string.replace(raw_str, r'\x2b', '+')
+            str_pkl = base64.decodestring(raw_str)
+            d_cvs = pickle.loads(str_pkl)
+
+        except:
+            error("UnPickling error")
+            sys.exit(0)
+
+        #-- start HTML output
+        header()
+
+        user_info(user, d_cvs)
+
+        log_msg(d_cvs)
+
+        import_info(d_cvs)
+        add_info(d_cvs)
+        remove_info(d_cvs)
+        modify_info(d_cvs)
+        
+        end_body()
+        sys.exit(0)
+
+    
+#############################################################################
+
