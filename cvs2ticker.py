c4491974b05fa19a3f07ab10e660379efced920e
Bill Segall <bill@segall.net>
2000-11-13 17:01:28 +1000

    python -> python1.6

--- cvs2ticker.py
+++ cvs2ticker.py
@@ -1,11 +1,11 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python1.6
 ########################################################################
 #
 #              Tickertape
 #              cvs loginfo producer
 #
 # File:        $Source: /home/d/work/personal/ticker-cvs/cvs2ticker/cvs2ticker.py,v $
-# Version:     $RCSfile: cvs2ticker.py,v $ $Revision: 1.17 $
+# Version:     $RCSfile: cvs2ticker.py,v $ $Revision: 1.18 $
 # Copyright:   (C) 1998-2000, David Leonard, Bill Segall & David Arnold.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -29,7 +29,7 @@ cvs2ticker - pass CVS loginfo messages through to tickertape
 
 """
 __author__ = 'David Leonard <david.leonard@dstc.edu.au>'
-__version__ = "$Revision: 1.17 $"[11:-2]
+__version__ = "$Revision: 1.18 $"[11:-2]
 
 
 ########################################################################
